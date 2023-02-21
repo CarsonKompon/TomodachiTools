d9f1a2e4d5acca4e6d52c19a9186474026e2aaff
Abdyy <82438230+AbdyyEee@users.noreply.github.com>
2023-02-21 18:30:21 +0400

    forgot the p lol

--- unpack.py
+++ unpack.py
@@ -20,7 +20,7 @@ match args[0]:
         bclyt = Bclyt(args[1])
     case "msbp":
         if len(args) < 2:
-            print("Usage: python unpack.py msbp <file.msb> [<output.json>]")
+            print("Usage: python unpack.py msbp <file.msbp> [<output.json>]")
             exit()
         msbp = Msbp(args[1])
         msbp.to_json(args[2])
